0df439db04135dd9e346f8d2273f33527803a8a2
Gabriel Capilla <170623949+gabrielcapilla@users.noreply.github.com>
2026-08-11 23:12:24 +0200

    chore: release Interlingua 2.0.1

--- src/components/molecules/Footer/AppFooter.tsx
+++ src/components/molecules/Footer/AppFooter.tsx
@@ -1,6 +1,7 @@
 import type { FC } from "react";
 
 const AUTHOR = "Gabriel Capilla";
+const AUTHOR_URL = "https://gabrielcapilla.github.io";
 const GITHUB_URL = "https://github.com/gabrielcapilla";
 
 export const AppFooter: FC = () => {
@@ -9,7 +10,17 @@ export const AppFooter: FC = () => {
   return (
     <footer className="app-footer">
       <p>
-        Interlingua v2.0.0 &copy; {year} | Crafted by {AUTHOR} |{" "}
+        Interlingua v2.0.1 &copy; {year} | Crafted by{" "}
+        <a
+          href={AUTHOR_URL}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="app-footer_link"
+          title={`Visit ${AUTHOR}'s website`}
+        >
+          {AUTHOR}
+        </a>{" "}
+        |{" "}
         <a
           href={GITHUB_URL}
           target="_blank"
